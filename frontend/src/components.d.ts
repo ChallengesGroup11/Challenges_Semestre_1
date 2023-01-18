c09383203f1f103a725908b036f2890ec98a1b14
Matteo Delandhuy <matteodelandhuypro@outlook.fr>
2023-01-18 17:35:44 +0100

    css

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -18,6 +18,7 @@ declare module '@vue/runtime-core' {
     EssentialLink: typeof import('./components/EssentialLink.vue')['default']
     FileUploader: typeof import('./components/FileUploader.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
+    LayoutHeader: typeof import('./components/LayoutHeader.vue')['default']
     ListRoom: typeof import('./components/ListRoom.vue')['default']
     ListUser: typeof import('./components/ListUser.vue')['default']
     OneSignalStatus: typeof import('./components/OneSignalStatus.vue')['default']
