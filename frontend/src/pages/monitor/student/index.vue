87db6cf6656a55d4d0754bbc91083ede862026f2
mattaiod <matteodelandhuydev@gmail.com>
2023-06-11 20:20:41 +0200

    done

--- frontend/src/pages/monitor/student/index.vue
+++ frontend/src/pages/monitor/student/index.vue
@@ -1,65 +1,106 @@
 <script setup lang="ts">
-
 const router = useRouter()
 
 const currentUser = reactive({ value: [] })
-// -> pour chaque élève, quand on clique, get tous ses créneaux avec (moniteur, élève, date, heure début, heure fin, statut, commentaires)
-onMounted(async () => {
-  if (localStorage.getItem('token') == null) {
-    await router.push('/auth')
-  } else {
-    await getUser()
-  }
-})
 
 const state = reactive({
   monitor: null as any,
-  student: [] as any,
+  students: [] as any,
 })
 
 const getUser = async () => {
   return fetch(`${import.meta.env.VITE_CHALLENGE_URL}/me`, {
     headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('token'),
+      Authorization: "Bearer " + localStorage.getItem("token"),
     },
   })
     .then((response) => response.json())
     .then((data) => {
-      currentUser.value = data;
+      currentUser.value = data
     })
     .catch((error) => {
-      console.error('Error:', error)
+      console.error("Error:", error)
     })
 }
 
 const loadData = async () => {
-  // Mattéo : Requete qui récupère les infos de l'élève ud monitor en connecter
+  if (localStorage.getItem("token") == null) {
+    await router.push("/auth")
+  } else {
+    await getUser()
+  }
+
   // LE QUERY BUILDER EST DANS MONITOR REPOSITORy et le controller c'est GetStudentByMonitorController
-  return fetch(`${import.meta.env.VITE_CHALLENGE_URL}/monitors/` + + currentUser.value?.monitor.id + `/student`, {
+  return fetch(`${import.meta.env.VITE_CHALLENGE_URL}/monitors/` + +currentUser.value?.monitor.id + `/student`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + localStorage.getItem("token"),
     },
   })
-  .then((response) => response.json())
+    .then((response) => response.json())
     .then((data) => {
-      state.student = data;
-      console.log(state.student)
+      // group by email
+
+      const slots = data
+
+      const slotsGroupByEmail = slots.reduce((acc, slot) => {
+        const email = slot.email
+        const existingSlot = acc[email]
+
+        if (existingSlot) {
+          existingSlot.push(slot)
+        } else {
+          acc[email] = [slot]
+        }
+
+        return acc
+      }, {})
+
+      // sort each group by date
+      const slotsGroupByEmailSorted = Object.keys(slotsGroupByEmail).reduce((acc, email) => {
+        const slots = slotsGroupByEmail[email]
+        const sortedSlots = slots.sort((a, b) => {
+          return new Date(a.date).getTime() - new Date(b.date).getTime()
+        })
+
+        acc[email] = sortedSlots
+
+        return acc
+      }, {})
+
+      state.students = slotsGroupByEmailSorted
+      console.log(state.students)
     })
     .catch((error) => {
-      console.error("Error:", error);
-    });
-
+      console.error("Error:", error)
+    })
 }
 
 loadData()
-
-
 </script>
 <template>
   <div>
-    <q-btn class="q-ma-md" color="secondary" label="Get Student" @click="loadData()" />
+    <q-card v-for="email in Object.keys(state.students)" key="student" class="mb-3 mt-3">
+      <q-card-section>
+        <q-item-label header
+          >{{ state.students[email][0].firstname }} {{ state.students[email][0].lastname }} ({{ email }})</q-item-label
+        >
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section>
+        <q-item-label header>Slots</q-item-label>
+        <q-item v-for="(slot, index) in state.students[email]" :key="slot.id">
+          <q-item-section>
+            <span> {{ index + 1 }} :</span>
+            <q-item-label>{{ slot.date }}</q-item-label>
+            <q-item-label>{{ slot.comment }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
