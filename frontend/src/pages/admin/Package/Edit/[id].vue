90cec13c15e1ec54682cf977904e7619d3f020b7
Océane <oceane.renoux@hotmail.fr>
2023-06-13 03:17:17 +0200

    fix

--- frontend/src/pages/admin/Package/Edit/[id].vue
+++ frontend/src/pages/admin/Package/Edit/[id].vue
@@ -49,11 +49,11 @@ const fetchOnePackage = async (id: string | string[]) => {
     })
 };
 
-const onSubmit = async (id: string) => {
+const onSubmit = async (id: string, e: { preventDefault: () => void; }) => {
+  e.preventDefault();
   const response = await fetch(`${import.meta.env.VITE_CHALLENGE_URL}/packages/` + id, {
     method: 'PATCH',
     headers: {
-      'Access-Control-Allow-Headers':'*',
       'Content-Type': 'application/merge-patch+json',
       'Authorization': 'Bearer ' + localStorage.getItem('token'),
     },
@@ -82,13 +82,13 @@ const onSubmit = async (id: string) => {
         <h2 class="text-h5 w-100 q-mb-xl">Editer un package</h2>
         <q-form @submit="onSubmit(packageDS.id)" class="q-gutter-md">
           <q-input v-model="packageDS.name" label="Nom du package" filled lazy-rules
-            :rules="[val => val && val.length > 0 || 'Veuillez écrire quelque chose']" />
+            :rules="[((val: string | any[]): string | any[]) => val && val.length > 0 || 'Veuillez écrire quelque chose']" />
           <q-input filled v-model="packageDS.description" label="Description" lazy-rules
-            :rules="[val => val && val.length > 0 || 'Veuillez écrire quelque chose']" />
+            :rules="[((val: string | any[]): string | any[]) => val && val.length > 0 || 'Veuillez écrire quelque chose']" />
           <q-input filled v-model.number="packageDS.nbCredit" label="Nombre de crédit" lazy-rules
-            :rules="[val => val && val > 0 || 'Veuillez écrire quelque chose']" />
+            :rules="[((val: number): number) => val && val > 0 || 'Veuillez écrire quelque chose']" />
           <q-input filled v-model.number="packageDS.price" label="Prix" lazy-rules
-            :rules="[val => val && val > 0 || 'Veuillez écrire quelque chose']" />
+            :rules="[((val: number): number) => val && val > 0 || 'Veuillez écrire quelque chose']" />
           <div>
             <q-btn label="Valider" type="submit" color="primary" />
           </div>
